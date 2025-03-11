39176c8f0542abc195d50dc12b93a61b70020eff
bwire <ahmedbwire17@gmail.com>
2025-03-11 07:03:55 +0300

    Adds class function

---
+++ index.js
@@ -0,0 +1,26 @@
+// Function that receives a callback function and calls it
+function receivesAFunction(callback) {
+    callback();
+}
+
+// Function that returns a named function
+function returnsANamedFunction() {
+    function namedFunction() {
+        console.log("This is a named function");
+    }
+    return namedFunction;
+}
+
+// Function that returns an anonymous function
+function returnsAnAnonymousFunction() {
+    return function() {
+        console.log("This is an anonymous function");
+    };
+}
+
+// Export the functions to be used in other files or tests
+module.exports = {
+    receivesAFunction,
+    returnsANamedFunction,
+    returnsAnAnonymousFunction
+};
